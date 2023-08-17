07919b8a85b9d4411955b10869517e7af472a654
Washington Irving <iamwashingyou@gmail.com>
2023-08-18 00:09:28 +0300

    fix lambda

--- netlify/functions/ac6-when/ac6-when.go
+++ netlify/functions/ac6-when/ac6-when.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	telegramapi "github.com/IrvingWash/ac6-when-got/internal/telegram-api"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	body := request.Body
+
+	var update telegramapi.TelegramUpdate
+
+	err := json.Unmarshal([]byte(body), update)
+
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Failed to read request body",
+		}, nil
+	}
+
+	telegramapi.SendMessage(&telegramapi.TelegramSendMessagePayload{
+		ChatID: update.Message.Chat.ID,
+		Text:   "Hello, World!",
+	})
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World!",
 	}, nil
 }
 
