b9ffb8b269e2321e0841d7c16f44aaf7712e31d5
Washington Irving <iamwashingyou@gmail.com>
2023-08-18 01:23:58 +0300

    fix tz

--- internal/ac6-count-down/ac6-count-down.go
+++ internal/ac6-count-down/ac6-count-down.go
@@ -2,19 +2,30 @@ package ac6countdown
 
 import (
 	"fmt"
+	"log"
 	"math"
 	"time"
 )
 
 func GetRemainingTime() string {
-	releaseDate := time.Date(2023, time.August, 25, 0, 0, 0, 0, time.Local)
+	location, err := time.LoadLocation("Europe/Moscow")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	releaseDate := time.Date(2023, time.August, 25, 0, 0, 0, 0, location)
 	currentTime := time.Now()
 
 	delta := releaseDate.Sub(currentTime)
 
 	days, hours := convertDelta(delta)
 
-	return fmt.Sprintf("Main system will engage combat mode in %v days, %v hours", days, hours)
+	return fmt.Sprintf(
+		"Main system will engage combat mode in %v days, %v hours",
+		days,
+		hours,
+	)
 }
 
 func convertDelta(delta time.Duration) (float64, float64) {
@@ -25,7 +36,7 @@ func convertDelta(delta time.Duration) (float64, float64) {
 
 	if hours > hoursInDay {
 		days = math.Floor(hours / hoursInDay)
-		hours = hours - days*hoursInDay
+		hours -= days * hoursInDay
 	}
 
 	return days, math.Floor(hours)
